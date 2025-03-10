3bdc3628d9f08f953a41b5391ccf049dd0ae40c0
Mohammed Hathim J <mohammedhathim0001@gmail.com>
2025-03-10 05:59:03 +0530

    Update app.py

--- 22PW26_CA2/app.py
+++ 22PW26_CA2/app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request
 import requests
-
+import os
 app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
@@ -107,7 +107,8 @@ def index():
  
 
 if __name__ == '__main__':
-    app.run(debug=True,port=8000)
+    port = int(os.environ.get("PORT", 5000))  
+    app.run(host="0.0.0.0", port=port)
 
 
 
